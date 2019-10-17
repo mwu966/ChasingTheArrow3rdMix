989818bc9bf0172125fe731612846d04fef45f4d
YukiAkase <y.akase.k@gmail.com>
2019-10-17 23:52:05 +0900

    マークダウンファイル読み込みサンプル

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,8 +37,11 @@ export default {
    */
   modules: [
     // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma'
+    '@nuxtjs/bulma',
+    '@nuxtjs/axios'
   ],
+  axios: {
+  },
   /*
    ** Build configuration
    */
--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@
             <h1 class="test-style">
               {{ name }}
               <nuxt-link to="/first-mix">1st</nuxt-link>
+              <div v-html="mark" />
             </h1>
           </div>
         </div>
@@ -18,12 +19,18 @@
 
 <script>
 import NavMenu from '~/components/NavMenu.vue'
+import marked from 'marked'
 export default {
   components: { NavMenu },
   data() {
     return {
-      name: 'てすとてすと'
+      name: 'てすとてすと',
+      mark: ''
     }
+  },
+  async asyncData({ app }) {
+    const response = await app.$axios.$get('content/test.md')
+    return { mark: marked(response) }
   }
 }
 </script>
