9a2acd5328277599fa7482250c42db16ee666cbb
YukiAkase <y.akase.k@gmail.com>
2019-12-08 18:12:28 +0900

    スマホ表示時のトップ画像位置調整

--- components/Top.vue
+++ components/Top.vue
@@ -9,13 +9,13 @@
 .hero
   @media screen and (max-width: 768px)
     background-image url('~assets/img/top/titleLogo.png'), url('~assets/img/top/jenny_mobile.png'), url('~assets/img/top/afro_mobile.png')
-    background-position bottom 10vh center, top 20vh left, top 20vh right
-    background-size 70vw, 90%, 60%
+    background-position top 45vh right, top 10vh left, top 10vh right
+    background-size 60vw, 90%, 60%
     background-repeat no-repeat
 
   @media screen and (min-width: 769px)
     background-image url('~assets/img/top/titleLogo.png'), url('~assets/img/top/jenny.png'), url('~assets/img/top/afro.png')
-    background-position bottom 10vh center, top 10vh left -30vw, top 10vh right -25vw
+    background-position bottom 5vh center, top 10vh left -30vw, top 10vh right -25vw
     background-size 80vmin, 90%, 65%
     background-repeat no-repeat
 </style>
