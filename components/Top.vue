167ce83d0f598d17098a955fcb27660015420c44
YukiAkase <y.akase.k@gmail.com>
2019-12-13 20:30:41 +0900

    サブタイトルの位置調整

--- components/Top.vue
+++ components/Top.vue
@@ -1,5 +1,7 @@
 <template>
-  <section class="hero is-fullheight-with-navbar"></section>
+  <section class="hero is-fullheight-with-navbar">
+    <div class="subtitle is-hidden-mobile"></div>
+  </section>
 </template>
 
 <script>
@@ -14,11 +16,21 @@
     background-repeat no-repeat
 
   @media screen and (min-width: 769px)
-    background-image url('~assets/img/top/titleLogo.png'), url('~assets/img/top/subtitle_desktop.svg'), linear-gradient(0, #EBCD28, #EBCD28), url('~assets/img/top/jenny.png'), url('~assets/img/top/afro.png')
-    background-position top 5vh center, bottom, bottom 4vh center, top 10vh left -15vw, top 10vh right -10vw
-    background-size 80vmin, 90vw, 100vw 10vh, 60%, 45%
+    background-image url('~assets/img/top/titleLogo.png'), url('~assets/img/top/jenny.png'), url('~assets/img/top/afro.png')
+    background-position top 5vh center, top 10vh left -15vw, top 10vh right -10vw
+    background-size 80vmin, 60%, 45%
     background-repeat no-repeat
     animation fadein 1.5s linear 1.5s
+
+.subtitle
+  position relative
+  top 75vh
+  width 100vw
+  height 100px
+  background-image url('~assets/img/top/subtitle_desktop.svg'), linear-gradient(0, #F1E442, #F1E442)
+  background-position center
+  background-size 95%, 100% 100%
+  background-repeat no-repeat
 
 @keyframes fadein
   from
